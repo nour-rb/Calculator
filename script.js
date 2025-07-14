bb6fb2e400a8d50dbda0a2863ba117c4960afaa3
nour roubhi <145440687+nour-rb@users.noreply.github.com>
2025-07-14 16:55:03 +0100

    feat: add basic math operation functions

--- script.js
+++ script.js
@@ -14,3 +14,23 @@ buttons.forEach(button => {
     button.classList.remove('dark');
   });
 });
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  if (b === 0) {
+    return "Can't divide by 0!";
+  }
+  return a / b;
+}
+
